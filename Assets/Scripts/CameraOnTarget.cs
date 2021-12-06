36cc2c9e01f029016a42c7172f48a18b2c95e61f
GuillaumeCharvet <guicharvet@orange.fr>
2021-12-06 13:33:18 +0100

    Camera

--- Assets/Scripts/CameraOnTarget.cs
+++ Assets/Scripts/CameraOnTarget.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class CameraOnTarget : MonoBehaviour
 {
-    private GameObject originalGameObject = GameObject.Find("MainObj");
-    public GameObject child = originalGameObject.transform.GetChild(0).gameObject;
+    //private GameObject originalGameObject;
+    //public GameObject child;
+
+    [Range(0f, 360f)]
+    public float rotationCam_x = 0.0f;
+    [Range(0f, 360f)]
+    public float rotationCam_y = 0.0f;
+    [Range(0f, 360f)]
+    public float rotationCam_z = 0.0f;
+
+    public Vector3 directionCamera { get { return new Vector3(rotationCam_x, rotationCam_y, rotationCam_z); } }
+
+    void Start()
+    {
+        //originalGameObject = GameObject.Find("MainObj");
+        //child = originalGameObject.transform.GetChild(0).gameObject;
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("coucou");
+        //Destroy(child);
+    }
 
     void Update()
     {
---
+++ Assets/Scripts/ClickObject.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickObject : MonoBehaviour
+{
+    public Vector3 targetLocation;
+    private Vector3 targetRotation;
+    private Vector3 positionCamera = new Vector3(+3, +4, +7);
+    private Transform cameraTransform { get; set; } = null;
+    private float cameraSpeed = 2.0f;
+
+    private void Start()
+    {
+        cameraTransform = GetComponent<Transform>();
+        targetLocation = cameraTransform.transform.position;
+    }
+
+    private void Update()
+    {
+        DrawRay();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, Camera.main.farClipPlane));
+
+            if (Physics.Raycast(cameraTransform.position, worldPos - cameraTransform.position, out RaycastHit hit))
+            {
+                Debug.Log(hit.transform.name);
+                targetLocation = hit.transform.position;
+                //////////CameraOnTarget tog = hit.GetComponent<CameraOnTarget>();
+                //Debug.Log(tog.rotationCam_x);
+                //Debug.Log(hit.rotationCam_x);
+                //targetRotation = new Vector3(hit.rotationCam_x, hit.rotationCam_y, hit.rotationCam_z);
+                Debug.Log(targetLocation);
+
+                //cameraTransform.position = targetLocation + positionCamera;
+            }
+        }
+        cameraTransform.position = Vector3.Lerp(cameraTransform.position, targetLocation + positionCamera, cameraSpeed * Time.deltaTime);
+        //Quaternion target = Quaternion.Euler(hit.)
+        //cameraTransform.rotation = Vector3
+
+    }
+
+    private void TransitionCamera(Vector3 start, Vector3 end)
+    {
+
+    }
+
+    private void DrawRay()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(mousePosition.x, mousePosition.y, Camera.main.farClipPlane));
+
+        Debug.DrawRay(cameraTransform.position, worldPos - cameraTransform.position, Color.red);
+    }
+}
