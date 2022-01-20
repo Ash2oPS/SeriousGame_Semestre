404a8956c49805ff9e11e3ec4bc534c65ad683c7
GuillaumeCharvet <guicharvet@orange.fr>
2022-01-20 11:34:22 +0100

    map + inventaire

--- Assets/Scripts/ClueManager.cs
+++ Assets/Scripts/ClueManager.cs
@@ -11,43 +11,56 @@ public class ClueManager : MonoBehaviour
     private int grid_column_width = 350;
     private int startingPos_grid_x = 500;
     private int startingPos_grid_y = 900;
+
     private int numberOfCluesFound;
 
     public List<GameObject> listOfClues;
     public List<GameObject> listOfCluesImages;
     public List<bool> listOfCluesBool;
+    public List<Vector2> listOfPositions;
+
+    public bool displayedJournal = false;
+    public Image imageJournal;
+
+    public bool displayedMap = false;
+    public Image imageMap;
 
     void Start()
     {
+        //image = GetComponentInChildren<Image>();
         for (int i = 0; i < listOfCluesImages.Count; i++)
         {
             listOfCluesBool.Add(false);
+            /*GameObject sprite = listOfCluesImages[i];
+            sprite.transform.position = new Vector3(listOfPositions[i].x, listOfPositions[i].y, 0);*/
         }
     }
 
     void Update()
     {
-        numberOfCluesFound = 0;
+        
+    }
+    public void ToggleJournal()
+    {
+        displayedJournal = displayedJournal ? false : true;
+        imageJournal.enabled = displayedJournal;
+
         for (int i = 0; i < listOfCluesBool.Count; i++)
         {
-            if (listOfCluesBool[i])
-            {
-                numberOfCluesFound++;
-                DisplayClueNumber(i, numberOfCluesFound-1);
-            }
-            else
-            {
-                listOfCluesImages[i].GetComponent<Image>().enabled = false;
-            }
+            DisplayClueNumber(i);
         }
     }
-
-    private void DisplayClueNumber(int i, int pos_number)
+    public void ToggleMap()
+    {
+        displayedMap = displayedMap ? false : true;
+        imageMap.enabled = displayedMap;
+    }
+    private void DisplayClueNumber(int i)
     {
         GameObject sprite = listOfCluesImages[i];
-        int row = pos_number / grid_width;
-        int column = pos_number % grid_width;
-        sprite.transform.position = new Vector3 (startingPos_grid_x + column * grid_column_width, startingPos_grid_y + row * grid_row_height, 0);
-        sprite.GetComponent<Image>().enabled = true;
+        //int row = pos_number / grid_width;
+        //int column = pos_number % grid_width;
+        //sprite.transform.position = new Vector3 (startingPos_grid_x + column * grid_column_width, startingPos_grid_y + row * grid_row_height, 0);
+        sprite.GetComponent<Image>().enabled = listOfCluesBool[i] && displayedJournal;
     }
 }
